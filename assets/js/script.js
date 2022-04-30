fc9e63be3321b3717fdcdac09df53b3b85a143a4
Suzanne Blake <suzanne.blake22@gmail.com>
2022-04-30 19:16:31 +0000

    Add comments to JS code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,30 @@
+//Starts the quiz
+
 function startQuiz() {
 
 }
 
+//Generates a random question
 function generateRandomQuestion() {
 
 }
 
+//Displays the question and the corresponding 4 answers
 function showQuestionAndAnswers() {
 
 }
 
+// Checks if the user's answer is right or wrong
 function checkAnswer() {
 
 }
 
-function increaseScore() {
+//Increases the user's score on a correct answer
+function incrementScore() {
 
 }
 
+//Show's the user's final score out of ten
 function showFinalScore() {
 
 }
