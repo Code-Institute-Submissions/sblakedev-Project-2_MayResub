5b280c4ecb9e2ad71bbab5f6ce1252069b4df7d7
Suzanne Blake <suzanne.blake22@gmail.com>
2022-04-30 20:16:14 +0000

    Add questions and answers to JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,140 @@
+//Questions
+
+let myQuestions = [
+    {
+        question: "What was the name of Uncle Vernon's company?",
+
+        answers: {
+            a:'Drills n Things',
+            b:'Dursley\'s Drills',
+            c:'Grunnings',
+            d:'Masons'
+        },
+
+        correctAnswer: 'c'
+    },
+    
+    {
+        question: "Who was headmaster of Hogwarts when Tom Riddle attended",
+
+        answers: {
+            a:'Albus Dumbledore'
+            b:'Armando Dippet'
+            c:'Dilys Derwent'
+            d:'Phineas Nigellus Black'
+        },
+
+        correctAnswer: 'b'
+    },
+
+    {
+        question: "How many Horcruxes did Lord Voldemort intend to make?",
+
+        answers: {
+            a:'5'
+            b:'6'
+            c:'7'
+            d:'8'
+        },
+
+        correctAnswer: 'b'
+    },
+
+    {
+        question: "What was Lord Voldemort's first Horcrux?",
+
+        answers: {
+            a:'Marvolo Gaunt\'s ring'
+            b:'Helga Hufflepuff\'s cup'
+            c:'Tom Riddle\'s diary'
+            d:'Nagini'
+        },
+
+        correctAnswer: 'c'
+    },
+
+    {
+        question: "Who revealed the existence of Dumbledore\'s Army to Dolores Umbridge?",
+
+        answers: {
+            a:'Marietta Edgecomb'
+            b:'Cho Chang'
+            c:'Michael Corner'
+            d:'Terry Boot'
+        },
+
+        correctAnswer: 'a'
+    },
+
+    {
+        question: "What does Harry do with the Elder Wand at the end of Deathly Hallows?",
+
+        answers: {
+            a:'He throws it away'
+            b:'He breaks it in half'
+            c:'He hides it behind Dumbledore\'s portrait'
+            d:'He keeps it to use himself'
+        },
+
+        correctAnswer: 'd'
+    },
+
+    {
+        question: "Who was the announcer at the Quidditch World Cup?",
+
+        answers: {
+            a:'Barty Crouch Sr'
+            b:'Cornelius Fudge'
+            c:'Rufus Scrimgeour'
+            d:'Ludo Bagman'
+        },
+
+        correctAnswer: ''
+    },
+
+    {
+        question: "",
+
+        answers: {
+            a:''
+            b:''
+            c:''
+            d:''
+        },
+
+        correctAnswer: ''
+    },
+
+    {
+        question: "",
+
+        answers: {
+            a:''
+            b:''
+            c:''
+            d:''
+        },
+
+        correctAnswer: ''
+    },
+
+    {
+        question: "",
+
+        answers: {
+            a:''
+            b:''
+            c:''
+            d:''
+        },
+
+        correctAnswer: ''
+    },
+]
+
+
+//Functions
+
 //Starts the quiz
 
 function startQuiz() {
@@ -5,26 +142,31 @@ function startQuiz() {
 }
 
 //Generates a random question
+
 function generateRandomQuestion() {
 
 }
 
 //Displays the question and the corresponding 4 answers
+
 function showQuestionAndAnswers() {
 
 }
 
 // Checks if the user's answer is right or wrong
+
 function checkAnswer() {
 
 }
 
 //Increases the user's score on a correct answer
+
 function incrementScore() {
 
 }
 
 //Show's the user's final score out of ten
+
 function showFinalScore() {
 
 }
